228122775713858fbb2fa7a5c65a876052d7b72f
Aditya-Panchal-FSdev100 <aditya.p@optimumfintech.co.in>
2024-03-14 12:19:43 +0530

    inital commit for send email project

---
+++ sendEmail/SendEmailWithGoogleSMTP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SendEmailWithGoogleSMTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string fromMail = "adityajsn1299@gmail.com";
+
+                string fromPassword = "fdmewihbwhndjfdc";
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(fromMail);
+                message.Subject = "Test Subject";
+                message.To.Add(new MailAddress("aditya.p@optimumfintech.co.in"));
+                message.Body = "<html><body> Test Body </body></html>";
+                message.IsBodyHtml = true;
+
+
+                var smtp = new System.Net.Mail.SmtpClient();
+                {
+                    smtp.Host = "smtp.office365.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+
+                    smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential(fromMail, fromPassword);
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Timeout = 600000;
+                }
+                smtp.Send(fromMail, "aditya.p@optimumfintech.co.in", message.Subject, message.Body);
+
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(fromMail, fromPassword),
+                    EnableSsl = true,
+                };
+
+                smtpClient.Send(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + e.StackTrace);
+            }
+        }
+    }
+}
